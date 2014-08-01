26ae2d7cf90d31a50d104b6f8f245d47e158cb01
muchanem <muchanem@gmail.com>
2014-07-31 20:35:06 -0400

    added questions

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+name = input("What is your name? ")
+if name == "Mark":
+   print("Coorect")
+elif name == "Jo":
+     print("you have a boring name? ")
+else:
+    print("coorect") 
+quest = input("What is your quest? ")
+
+
+color = input("What is your favorite color? ")
+
+print("You answered")
+print("Your name is  {}".format(name))
+print("Your quest is {}".format(quest))
+print("Your color is {}".format(color))
+
