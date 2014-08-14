30fe4bf1c19681962b3e0dbb265aed4eceb6bab5
muchanem <muchanem@gmail.com>
2014-08-13 20:53:11 -0400

    Finished Questions

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("hello world")
+
 
 name = input("What is your name? ")
 if name == "Mark":
@@ -12,6 +12,11 @@ quest = input("What is your quest? ")
 
 color = input("What is your favorite color? ")
 
+capital of acedia = input("What is the capital of Acidea? ")
+print("Incorrect")
+asped volicity of a unlegged swallow = ("what is the asped volicity of a unlegged swallow? ")
+if asped volicity of a unlegged swallow == ("European or African")
+   print("I do not know that ahhh") 
 print("You answered")
 print("Your name is  {}".format(name))
 print("Your quest is {}".format(quest))
